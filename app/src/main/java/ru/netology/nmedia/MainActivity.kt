0c10fd4767cd2c06438373811ebcb69788d5a072
Sergei Sokolov <sergsok1984@gmail.com>
2023-01-18 19:51:38 +0300

    add MVVM to Lesson5

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,7 +2,9 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.viewmodel.PostViewModel
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
@@ -13,44 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "14 января в 10:15",
-            likedByMe = false
-        )
-
-        binding.apply {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            if (post.likedByMe) {
-                likes?.setImageResource(R.drawable.ic_likes_24dp)
-            }
-            likesCount?.text = increment(post.likes)
-            shareCount?.text = increment(post.share)
-            viewsCount?.text = increment(post.views)
-
-            likes?.setOnClickListener {
-                post.likedByMe = !post.likedByMe
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            binding.apply {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
                 likes.setImageResource(
                     if (post.likedByMe) R.drawable.ic_liked_24dp else R.drawable.ic_likes_24dp
                 )
-                if (post.likedByMe) post.likes++ else post.likes--
-                likesCount?.text = increment(post.likes)
-            }
-            share?.setOnClickListener {
-                post.share++
-                shareCount?.text = increment(post.share)
-            }
-            views?.setOnClickListener {
-                post.views++
-                viewsCount?.text = increment(post.views)
+                likesCount.text = increment(post.likes)
+                shareCount.text = increment(post.share)
+                viewsCount.text = increment(post.views)
             }
         }
+        binding.likes.setOnClickListener {
+            viewModel.like()
+        }
+        binding.share.setOnClickListener {
+            viewModel.share()
+        }
+        binding.view.setOnClickListener {
+            viewModel.view()
+        }
     }
-
 
     private fun increment(count: Int): String {
         return if (count in 1000..1099) {
